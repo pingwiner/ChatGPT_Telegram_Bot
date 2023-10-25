7c33c6fa467f62c83c999086eeb29982f30f3eee
pingwiner <i486dx@yandex.ru>
2023-10-25 12:28:07 +0400

    Daemon added

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"	
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	openai "github.com/sashabaranov/go-openai"
+	"github.com/sevlyar/go-daemon"
 )
 
 var client *openai.Client
@@ -50,15 +51,41 @@ func gptRequest(userName string, message string) string {
 	fmt.Println(resp.Choices[0].Message.Content)
 	userReq.Messages = append(userReq.Messages, resp.Choices[0].Message)
 	req[userName] = userReq
+	if (len(userReq.Messages) > 10) {
+	    delete(req, userName)
+	}
 	return result
 }
 
 func main() {
+	cntxt := &daemon.Context{
+		PidFileName: "justaskme.pid",
+		PidFilePerm: 0644,
+		LogFileName: "justaskme.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[go-daemon justaskme]"},
+	}
+
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		log.Fatal("Unable to run: ", err)
+	}
+	if d != nil {
+		return
+	}
+	defer cntxt.Release()
+
+	log.Print("- - - - - - - - - - - - - - -")
+	log.Print("daemon started")
+
 	Init()
 
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_KEY"))
-	if err != nil {
-		log.Panic(err)
+	bot, err1 := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_KEY"))
+	if err1 != nil {
+		log.Panic(err1)
 	}
 	
 	bot.Debug = true
